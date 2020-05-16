c923dd853bf0a76e9ae202146a12168b563638e5
Kalana Vimukthi Weerarathna <kalana.weerarathne@scicom.com.my>
2020-05-16 20:22:20 +0530

    -initial commit with db configurations

---
+++ src/test/java/com/vpark/vparkservice/VparkServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.vpark.vparkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VparkServiceApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
