a2f4b6789472c59fb07701e660d3ac9ae94a8689
Nitish <Nitish.bansal@gaussnetworks.com>
2020-06-15 16:32:44 +0530

    no message

---
+++ src/main/java/com/vpark/vparkservice/entity/UserWallet.java
@@ -0,0 +1,37 @@
+package com.vpark.vparkservice.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.DynamicUpdate;
+import lombok.Data;
+
+@Data
+@Entity
+@DynamicUpdate
+@Table(name = "USER_WALLET")
+public class UserWallet extends Savable {
+	
+	@ManyToOne(cascade = CascadeType.DETACH)
+	@JoinColumn(name = "USER_ID", referencedColumnName = "ID")
+	private User user;
+	
+	@Column(name = "DEPOSIT_AMT")
+	private double deposit ;
+	
+	@Column(name = "REAL_AMT")
+	private double real ;
+	
+	@Column(name = "BONUS_AMT")
+	private double bonus ;
+	
+
+	
+
+
+	
+
+}
